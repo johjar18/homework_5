dbd9d95066b3bf1570e5e0483ee865cd5db04a15
Jaren Johnson <johjar@umich.edu>
2018-02-11 01:43:11 -0500

    Adding updated file

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -3,7 +3,7 @@ import json
 import sys
 import requests
 import secret_data # file that contains OAuth credentials
-# import nltk # uncomment line after you install nltk
+import nltk
 
 ## SI 206 - HW
 ## COMMENT WITH:
@@ -32,20 +32,34 @@ requests.get(url, auth=auth)
 #Code for Part 1:Get Tweets
 baseurl= 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 params= {'screen_name': username, 'count': num_tweets}
-response_obj= requests.get(baseurl, params, auth=auth)
-# print(response)
-response_string= response_obj.json() #converting result which exist as Response object into Python file
-# print(json.dumps(response_string, indent=4))
+response= requests.get(baseurl, params, auth=auth)
+json_data= json.loads(response.text)
+# print(json.dumps(json_data, indent=4))
 
 new_file= open('tweet.json', 'w')
-new_file.write(json.dumps(response_string, indent= 4))
+new_file.write('json_data')
 new_file.close()
-
+# print(json.dumps(json_data, indent=4))
 
 
 
 
 #Code for Part 2:Analyze Tweets
+for tweet_data in json_data:
+    tweet_text= tweet_data['text']
+    # print(tweet_text)
+
+tokens = nltk.word_tokenize(tweet_text)
+freqDist = nltk.FreqDist(token for token in tokens if token.isalpha() and "http" not in token)
+
+for word, frequency in freqDist.most_common(5):
+    freq= word + " " + str(frequency)
+    print(freq)
+
+print("USER:" + username)
+print("TWEETS ANALYZED:" + " " + num_tweets)
+print("5 MOST FREQUENT WORDS:" + " " + (freq))
+
 
 
 
